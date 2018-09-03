63694cc41f3a13382c27be8d378f916774f162e1
Jonathan <jonathanconn29@gmail.com>
2018-09-03 16:34:57 -0400

    creating linechart

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.layout.BorderPane;
 
 
@@ -13,6 +15,17 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	
+			NumberAxis yAxis = new NumberAxis();  
+			yAxis.setLabel("y Axis");
+			
+			NumberAxis xAxis = new NumberAxis();  
+			xAxis.setLabel("x Axis");
+			
+			LineChart linechart = new LineChart(xAxis, yAxis);
+	
+			
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
